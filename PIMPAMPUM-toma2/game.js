75daef7183ddb4489c0e3d952bc7f71edc77771f
Andrés <116580236+MrTheory4@users.noreply.github.com>
2023-11-24 10:36:32 +0100

    Update game.js
    Agregados los personajes al escenario

--- PIMPAMPUM-toma2/game.js
+++ PIMPAMPUM-toma2/game.js
@@ -11,7 +11,7 @@ var config = {
 physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 300 },
+            gravity: { y: 0 },
             debug: false
         }
     },
@@ -30,16 +30,23 @@ function preload ()
     this.load.image('Desierto', 'resources/Fondo1.png');
     
     this.load.image('Vias', 'resources/tren.png');
+
+    this.load.image('vaquero', 'resources/vaquero.png');
+    this.load.image('vaquero_2', 'resources/vaquero_2.png');
 }
 
 function create ()
 {
      this.add.image(WIDTH/2, HEIGHT/2, 'Desierto');
     
-    //this.add.image(0, 0, 'Vías').SetOrigin(0, 0);
+    // Agregamos las vías al escenario
     vias = this.physics.add.staticGroup();
     vias.create(WIDTH/2, HEIGHT/2, "Vias");
-    
+
+    /Agregamos los vaqueros
+    vaquero_1 = this.physics.add.sprite(WIDTH/4, HEIGHT/4,'vaquero');
+    vaquero_2 = this.physics.add.sprite(3*WIDTH/4,3*HEIGHT/4,'vaquero_2');
+    player.setCollideWorldBounds(true);
 
 }
 
