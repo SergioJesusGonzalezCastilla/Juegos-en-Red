4b1370a140b186645e857ba194116329b54fee28
Sergio Jesús González Castilla <146626099+SergioJesusGonzalezCastilla@users.noreply.github.com>
2023-11-24 14:20:52 +0100

    Update game.js
    Introduccion de tanque, carreta1, carreta2, mesa como collides.
     mejora en la organización
    
    creación de texto (no se ve aun en pantalla)
    
    implementación de variable vida

--- PIMPAMPUM-toma2/game.js
+++ PIMPAMPUM-toma2/game.js
@@ -1,4 +1,14 @@
 var vias;
+var tanque;
+var carreta1;
+var carreta2;
+var mesa;
+
+var player;
+
+
+var texto;
+var vida = 3;
 
 const WIDTH = 1280;
 const HEIGHT = 720;
@@ -27,26 +37,57 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.image('Desierto', 'resources/Fondo1.png');
+    //FONDO
+    this.load.image('Desierto', 'resources/Fondo Desierto.png');    
     
-    this.load.image('Vias', 'resources/tren.png');
+    //ELEMENTOS DEL MUNDO
+    this.load.image('Vías','resources/Vias de Tren.png');
+    this.load.image('Tanque','resources/Agüita.png');
+    this.load.image('Carreta1','resources/Carreta derecha.png');
+    this.load.image('Carreta2','resources/Carreta izquierda.png');
+    this.load.image('Mesa','resources/Mesa.png');
 
+    //PLAYERS
     this.load.image('vaquero', 'resources/vaquero.png');
     this.load.image('vaquero_2', 'resources/vaquero_2.png');
+
+
 }
 
 function create ()
 {
-     this.add.image(WIDTH/2, HEIGHT/2, 'Desierto');
     
-    // Agregamos las vías al escenario
+//FONDO
+    this.add.image(WIDTH/2, HEIGHT/2, 'Desierto');   
+    
+    //ELEMENTOS DEL MUNDO
     vias = this.physics.add.staticGroup();
-    vias.create(WIDTH/2, HEIGHT/2, "Vias");
+    this.add.image(WIDTH/2, HEIGHT/2, 'Vías').setScale(1/2);
+    
+    tanque = this.physics.add.staticGroup();
+    this.add.image(WIDTH/2, HEIGHT/2, 'Tanque').setScale(1/2);
 
-    /Agregamos los vaqueros
+    carreta1 = this.physics.add.staticGroup();
+    this.add.image(WIDTH/2, HEIGHT/2, 'Carreta1').setScale(1/2);
+
+    carreta2 = this.physics.add.staticGroup();
+    this.add.image(WIDTH/2, HEIGHT/2, 'Carreta2').setScale(1/2);
+
+    mesa = this.physics.add.staticGroup();
+    this.add.image(WIDTH/2, HEIGHT/2, 'Mesa').setScale(1/2);
+
+
+    //Agregamos los vaqueros
     vaquero_1 = this.physics.add.sprite(WIDTH/4, HEIGHT/4,'vaquero');
     vaquero_2 = this.physics.add.sprite(3*WIDTH/4,3*HEIGHT/4,'vaquero_2');
     player.setCollideWorldBounds(true);
+
+    //Texto
+    texto = this.add.text(16, 16, 'vida:', {
+        fontSize: '200px',
+        fill: '#000'
+    })
+    
 
 }
 
