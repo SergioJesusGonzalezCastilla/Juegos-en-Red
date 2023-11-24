39be6452098572c49cbc862660548a3fcf7189d8
Sergio Jesús González Castilla <146626099+SergioJesusGonzalezCastilla@users.noreply.github.com>
2023-11-24 09:56:40 +0100

    Update game.js
    Añadidas las fisicas
    introducida la via central

--- PIMPAMPUM-toma2/game.js
+++ PIMPAMPUM-toma2/game.js
@@ -4,7 +4,14 @@ var config = {
     type: Phaser.AUTO,
     width:2048,
     height:1152,
-
+    
+physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -19,14 +26,17 @@ function preload ()
 {
     this.load.image('Desierto', 'resources/Fondo1.png');
     
-    this.load.image('Vías','resources/Vias de Tren-png');
+    this.load.image('Vías', 'resources/tren.png');
 }
 
 function create ()
 {
     this.add.image(1023, 576, 'Desierto').SetOrigin(0, 0)
     
-    this.add.image(400, 300, 'Vías');
+    this.add.image(800, 400, 'Vías').SetOrigin(0, 0);
+    vias = this.physics.add.staticGroup();
+
+    platforms.create(300,300, 'vias');
     
 
 }
