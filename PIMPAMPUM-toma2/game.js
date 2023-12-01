4f857afb3074e295cffcb42e84ec46382c57acc2
Javier169 <146627277+Javier169@users.noreply.github.com>
2023-12-01 15:56:39 +0100

    Se agregan corazones al escenario en función de la cantidad total de vida perdida

--- PIMPAMPUM-toma2/game.js
+++ PIMPAMPUM-toma2/game.js
@@ -463,6 +463,16 @@ export class Game extends Phaser.Scene{
                 }
             }
         });
+        //Comprobaremos la cantidad total de vida perdida para crear los objetos corazón en función de la misma
+        if(vida_total_perdida===70)
+        {
+            corazon_1.create(WIDTH/2, 2*HEIGHT/6, 'corazon');
+        }
+
+        if(vida_total_perdida===110)
+        {
+            corazon_2.create(WIDTH/2, 4*HEIGHT/6, 'corazon');
+        }
     }
 
 }
