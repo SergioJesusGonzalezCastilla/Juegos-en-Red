d3c1d0dc83be5d47d80682afd7e7dcb4004a9221
Sergio Jesús González Castilla <146626099+SergioJesusGonzalezCastilla@users.noreply.github.com>
2023-12-03 20:44:11 +0100

    Boton bien puesto mapa.js

--- PIM PAM PUM!/mapa.js
+++ PIM PAM PUM!/mapa.js
@@ -21,57 +21,6 @@ export class Mapas extends Phaser.Scene{
         sonidoFondo = this.sound.add('sonidoFondo');
         sonidoDisparo = this.sound.add('sonidoDisparo');
         sonidoFondo.loop = true;
-        
-        const volver = this.add.sprite(1280/2, 720/2, 'botonVolver').setInteractive();
-        
-        // Botón VOLVER
-        // Define las animaciones del botón
-          this.anims.create({
-              key: 'buttonNormal4',
-              frames: this.anims.generateFrameNumbers('botonVolver', { start: 0, end: 0 }),
-              frameRate: 1,
-              repeat: 0
-          });
-
-          this.anims.create({
-              key: 'buttonHover4',
-              frames: this.anims.generateFrameNumbers('botonVolver', { start: 1, end: 1 }),
-              frameRate: 1,
-              repeat: 0
-          });
-
-          this.anims.create({
-              key: 'buttonClick4',
-              frames: this.anims.generateFrameNumbers('botonVolver', { start: 2, end: 2 }),
-              frameRate: 1,
-              repeat: 0
-          });
-
-          // Configura la interactividad del botón
-          volver.on('pointerover', () => {
-            volver.play('buttonHover4');
-          });
-
-          volver.on('pointerout', () => {
-            volver.play('buttonNormal4');
-          });
-
-          volver.on('pointerdown', () => {
-            volver.play('buttonClick4');
-              //meter tiempo espera
-              this.scene.transition({
-                  target: 'seleccion',
-                  duration:1000,
-              });
-                sonidoFondo.stop();
-                sonidoDisparo.play();
-          });
-
-          volver.on('pointerup', () => {
-            volver.play('buttonHover4');
-          });
-
-
 
           const vaqueros = this.add.sprite(352, 190, 'botonVaqueros').setInteractive();
 
@@ -121,6 +70,56 @@ export class Mapas extends Phaser.Scene{
         vaqueros.on('pointerup', () => {
             vaqueros.play('buttonHoverV4');
         });
+
+
+         const volver = this.add.sprite(1280/2, 720/2, 'botonVolver').setInteractive();
+        
+        // Botón VOLVER
+        // Define las animaciones del botón
+          this.anims.create({
+              key: 'buttonNormal4',
+              frames: this.anims.generateFrameNumbers('botonVolver', { start: 0, end: 0 }),
+              frameRate: 1,
+              repeat: 0
+          });
+
+          this.anims.create({
+              key: 'buttonHover4',
+              frames: this.anims.generateFrameNumbers('botonVolver', { start: 1, end: 1 }),
+              frameRate: 1,
+              repeat: 0
+          });
+
+          this.anims.create({
+              key: 'buttonClick4',
+              frames: this.anims.generateFrameNumbers('botonVolver', { start: 2, end: 2 }),
+              frameRate: 1,
+              repeat: 0
+          });
+
+          // Configura la interactividad del botón
+          volver.on('pointerover', () => {
+            volver.play('buttonHover4');
+          });
+
+          volver.on('pointerout', () => {
+            volver.play('buttonNormal4');
+          });
+
+          volver.on('pointerdown', () => {
+            volver.play('buttonClick4');
+              //meter tiempo espera
+              this.scene.transition({
+                  target: 'seleccion',
+                  duration:1000,
+              });
+                sonidoFondo.stop();
+                sonidoDisparo.play();
+          });
+
+          volver.on('pointerup', () => {
+            volver.play('buttonHover4');
+          });
 
     }
 
