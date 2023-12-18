2c25e9d149dafc5de596e5a76b0b655d7830dcaa
Andrés <116580236+MrTheory4@users.noreply.github.com>
2023-12-18 17:20:58 +0100

    Update UserService.java

--- Spring/UserService.java
+++ Spring/UserService.java
@@ -1,141 +1,144 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
-@RestController
-@RequestMapping("/usuarios") //Utilizaremos la misma raíz del directorio para todos los métodos de esta clase
-public class UserController {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
-	//Crearemos un objeto de la clase UserService con la que utilizaremos sus métodos 
-	//Usaremos Autowired para que se enlace automáticamente
+@Service
+public class UserService {
+
+	//Crearemos un HashMap en el que almacenaremos el conjunto de usuarios, identificándolos por su nombre
+	private ConcurrentHashMap<String,User> usuarios = new ConcurrentHashMap<>();
+	//Cada vez que un usuario se de de alta, se almacenará en el String su nombre, y User el conjunto de datos del usuario
 	
-	@Autowired
-	private UserService User_Service;
+	//Se define también una variable que almacenará el número de usuarios
+	private int numUsuarios=0;
 	
-	//Primero determinaremos un método con el que registrarse
-	//Se tratará de una operación de tipo post, pues crearemos un nuevo objeto
-	
-	@PostMapping
-	public ResponseEntity<User> signUp(@RequestBody User usuario)
+	//Método get del numero de usuarios conectados
+	public int getNumUsuarios()
 	{
-		//Primero añadiremos a User_Service el usuario, en caso de que no exista uno igual
-		//Nos devuelve un boolean, con  el que podemos lanzar un mensaje para indicar si se ha registrado o no
-		boolean usuarioRepetido=User_Service.registrarse(usuario);
-		//En función del valor obtenido, lanzamos uno u otro mensaje
-		if(usuarioRepetido==true)
-		{
-			//En caso de que se halla creado, devolveremos un indicador de que se ha creado con éxito
-			// También se devuelve el correspondiente usuario con sus datos
-			return new ResponseEntity<>(usuario, HttpStatus.OK);
+		return numUsuarios;
+	}
+	
+	//Método get del total de usuarios conectados
+	public List<User> getUsuarios()
+	{
+		List<User> conjuntoUsers= new ArrayList<>();
+		for(String key : usuarios.keySet()) {
+			conjuntoUsers.add(getUser(key));
 		}
+		return conjuntoUsers;
+	}
+	//Definimos ahora un método que se encargará de dar de alta a nuestros usuarios
+	//Como ya hemos definido en otras clases, se devolverá un boolean que será verdadero en caso de que se lleve a cabo el registro
+	//Y será falso en caso de que ya exista dicho usuario y no pueda registrarse
+	
+	public boolean registrarse(User usuario)
+	{
+		//En caso de que uno de los elementos del conjunto de usuarios ya tenga el mismo nombre, no se crea el usuario
+		if(usuarios.containsKey(usuario.getUsuario()))
+		{
+			return false;
+		}
+		//En caso contrario, si que dejaría registrarse
 		else
 		{
-			//En caso de que no haya podido crearse se devuelve un mensaje de error
-			//Este indicará al usuario que se ha llevado a cabo de forma incorrecta la petición
-			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+			//Por lo tanto, el nuevo usuario registrado pasaría a tener su campo registrado a true
+			usuario.setRegistrado(true);
+			//También se agregaría el correspondiente usuario al conjunto que tiene el mapa, medinate el método put
+			//Como key se asignaría el nombre de usuario
+			usuarios.put(usuario.getUsuario(),usuario);
+			return true;
+		}
+	}
+		
+	//Definimos ahora un método que nos devolverá el usuario en caso de que se encuentre entre el conjunto de usuarios registrados a partir de su nombre
+	public User getUser(String nombre_usuario)
+	{
+		//En caso de que no exista
+		if(!usuarios.containsKey(nombre_usuario))
+		{
+			return null;
+		}
+		//En caso de que exista
+		else
+		{
+			return usuarios.get(nombre_usuario);
 		}
 	}
 	
-	//Ahora determinaremos un método con el que iniciar sesión
-	//Se tratará de una operación de tipo post, pues solo requeriremos de un usuario como cuerpo de la petición para obtener el resultado requerido
-	
-	@GetMapping
-	public ResponseEntity<User> logIn(@RequestBody User usuario)
+	//Ahora, definimos un método que permite que los usuarios inicien sesión a partir de un nombre de usuario y una contraseña
+	public User inicio_sesion(User usuario)
 	{
-		//Se comprobará mediante el método correspondiente de User_Service que usuario y contraseña coinciden
-		//Nos devuelve un User, con  el que podemos lanzar un mensaje para indicar si se ha logeado
-		User usuarioObtenido=User_Service.inicio_sesion(usuario);
-		//En función del valor obtenido, lanzamos uno u otro pensaje
-		if(usuarioObtenido!=null)
+		//Primero comprobamos el hipotético caso de que no haya rellenado el nombre de usuario ni la contraseña
+		if(usuario.getUsuario()==null || usuario.getPassword()==null)
 		{
-			//En caso de que se halla accedido correctamente, devolveremos un indicador de que se ha creado con éxito
-			// También se devuelve el correspondiente usuario con sus datos
-			return new ResponseEntity<>(usuario, HttpStatus.OK);
+			return null;
 		}
+		//Ahora en caso de que si lo haya hecho
 		else
 		{
-			//En caso de que no haya podido obtenerse se devuelve un mensaje de error
-			//Este indicará al usuario que se ha llevado a cabo de forma incorrecta la petición
-			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		}
-	}
-	
-	/*
-	//Ahora determinaremos un método con el que cerrar sesión
-	//Se tratará de una operación de tipo post, pues solo requeriremos de un usuario como cuerpo de la petición
-	@PostMapping(value="/logOut")
-	public ResponseEntity<User> logOut(@RequestBody User usuario)
-	{
-		//Se comprobará mediante el método correspondiente de User_Service que usuario y contraseña coinciden
-		//Nos devuelve un boolean, con  el que podemos lanzar un mensaje para indicar si se ha logeado
-		boolean usuarioObtenido=User_Service.deleteUser(usuario.getUsuario(),usuario.getPassword());
-		//En función del valor obtenido, lanzamos uno u otro pensaje
-		if(usuarioObtenido==true)
-		{
-			//En caso de que se halla accedido correctamente, devolveremos un indicador de que se ha creado con éxito
-			// También se devuelve el correspondiente usuario con sus datos
-			return new ResponseEntity<>(usuario, HttpStatus.OK);
-		}
-		else
-		{
-			//En caso de que no haya podido obtenerse se devuelve un mensaje de error
-			//Este indicará al usuario que se ha llevado a cabo de forma incorrecta la petición
-			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		}
-	}
-	
-	//Ahora determinaremos un método que nos permitirá visualizar la información de un jugador a partir de su ID
-	@GetMapping(value="/playerInfo")
-	public ResponseEntity<String> playerInfo(@PathVariable String nombre)
-	{
-		User usuarioObtenido= User_Service.getUser(nombre);
-		//Ahora devolveremos una u otra información en función de si el usuario existe o no
-		if(usuarioObtenido!=null)
-		{
-			return new ResponseEntity<>(usuarioObtenido.toString(),HttpStatus.OK);
-		}
-		//En caso de que no se haya conseguido se devuelve un mensaje de error
-		//Este indicará al usuario que se ha llevado a cabo de forma incorrecta la petición
-		else
-		{
-			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		}
-	}
-	
-	//A continuación, se definirá un método que permititrá cambiar la contraseña de un jugador
-	@PutMapping(value="/changePassword")
-	public ResponseEntity<User> changePassword(@PathVariable String nombre, @RequestBody User usuario)
-	{
-		User usuarioObtenido= User_Service.getUser(nombre);
-		//Ahora devolveremos una u otra ifnormación en caso de que el usuario exista o no
-		if(usuarioObtenido!=null)
-		{
-			//Ahora comprobaremos si coinciden las contraseñas o no
-			if(usuario.getPassword().equals(usuarioObtenido.getPassword()))
+			User usuarioObtenido= getUser(usuario.getUsuario());
+			//Si el usuario existe, es decir, no hemos obtenido un null
+			if(usuarioObtenido!=null)
 			{
-				usuarioObtenido.setPassword(usuario.getPassword());
-				return new ResponseEntity<>(usuarioObtenido,HttpStatus.OK);
+				//Ahora que hemos obtenido este usuario, tenemos que comprobar que la contraseña coincide
+				if(usuarioObtenido.getPassword().equals(usuario.getPassword()))
+				{
+					numUsuarios++;
+					return usuarioObtenido;
+				}
+				//En caso contrario
+				else
+				{
+					return null;
+				}
 			}
+			//En caso contrario
 			else
 			{
-				return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+				return null;
 			}
 		}
+	}
+	
+	//Definimos ahora un método que borrará el usuario en caso de que se encuentre entre el conjunto de usuarios registrados a partir de su nombre
+	//Devolverá un booleano que indicará si se ha llevado a cabo el borrado o no
+	public boolean deleteUser(String nombre_usuario, String psw)
+	{
+		//Primero comprobamos el hipotético caso de que no haya rellenado el nombre de usuario ni la contraseña
+		if(nombre_usuario==null || psw==null)
+		{
+			return false;
+		}
+		//En caso de que si lo haya hecho
 		else
 		{
-			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		}
-		
+			User usuarioObtenido= getUser(nombre_usuario);
+			//Si el usuario existe, es decir, no hemos obtenido un null
+			if(usuarioObtenido!=null)
+			{
+				//Ahora que hemos obtenido este usuario, tenemos que comprobar que la contraseña coincide
+				if(usuarioObtenido.getPassword().equals(psw))
+				{
+					usuarioObtenido.setRegistrado(false);
+					usuarios.remove(nombre_usuario);
+					numUsuarios--;
+					return true;
+				}
+				//En caso contrario
+				else
+				{
+					return false;
+				}
+			}
+			//En caso contrario
+			else
+			{
+				return false;
+			}
+		}	
 	}
-
-	//Definimos un método ahora que nos permite obtener el número de usuarios como si se tratase de un string
-	@GetMapping(value="/numUsuarios")
-	public String numUsers()
-	{
-		return "Hay "+ User_Service.getNumUsuarios()+ " usuarios conectados";
-	}
-	*/
 }
