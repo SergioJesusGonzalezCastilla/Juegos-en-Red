f21c7ad8a7836d233d5de5f602d11bcf9de8ce2a
Andrés <116580236+MrTheory4@users.noreply.github.com>
2023-12-18 16:03:52 +0100

    Actualizada la clase User del servidor
    Se han eliminado algunos parámetros y métodos que no van a utilizarse

--- Spring/User.java
+++ Spring/User.java
@@ -3,21 +3,18 @@ package com.example.demo;
 public class User {
 	
 	//Definimos las variables que determinarán las carcaterísticas de un usuario
-	private boolean registrado; //Booleano en que se indicará si el usuario ya forma parte dle sistema
 	private String nombre; //Nombre del usuario
 	private String password; //Contraseña
-	private float puntuacion; //Puntuación para clasificarlo
+	private boolean registrado; //Booleano en que se indicará si el usuario ya forma parte del sistema
 	
 	//Definimos ahora el constructor con el que se definirá un usuario al registrarse
 	public User(String nombre_usuario, String password_usuario)
 	{
-		//Primero asiganremos un valor por defecto al parámetro registrado
-		registrado=false;
 		//Asignamos los valores que se reciben al crear un usuario a las variables correspondientes
 		nombre=nombre_usuario;
 		password=password_usuario;
-		//Al crear un usuario, se inicializará su correspondiente puntuación a 0
-		puntuacion=0;
+		//Asignaremos un valor por defecto al parámetro registrado
+		registrado=false;
 	}
 	
 	//Se definen ahora los métodos get correspondientes para poder obtener la infromación del usuario
@@ -32,10 +29,6 @@ public class User {
 	public String getPassword()
 	{
 		return this.password;
-	}
-	public float getPuntuacion()
-	{
-		return this.puntuacion;
 	}
 	
 	//Se definen ahora lo métodos set correspondientes, con los que se modificará la información relativa al usaurio
@@ -68,28 +61,5 @@ public class User {
 			this.password=password_param;
 			return true; //En caso de que no coincidan, se modifica el valor
 		}
-	}
-	public boolean setPuntuacion(float puntuacion_param)
-	{
-		//Primero nos aseguraremos de que el usario no tenga el mismo valor para puntuacion que el parámetro que se está pasando, en cuyo caso no tendría sentido cambiar este parámetro
-		if(this.puntuacion==puntuacion_param)
-		{
-			return false; //No se ha modificado el valor
-		}
-		else
-		{
-			this.puntuacion=puntuacion_param;
-			return true; //En caso de que no coincidan, se modifica el valor
-		}
-	}
-	
-	//EL MÉTODO SIGUIENTE PODRÍA SER DE UTILIDAD, AUNQUE QUIZÁS PUEDA BORRARSE AL FINAL DE LA FASE!!!!
-	//Definimos un método con la que podamos pasar a String el contenido de un usuario
-	//Para ello hay que hacer override
-	
-	@Override
-	public String toString() 
-	{
-		return "Usuario: Nombre=" + this.nombre + ", contraseña=" +this.password + ", Puntuación: " +this.puntuacion;
 	}
 }
