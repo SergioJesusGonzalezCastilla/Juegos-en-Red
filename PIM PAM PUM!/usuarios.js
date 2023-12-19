9a942a1953c314e892c5e7192653ef6aa83e7a18
Andrés <116580236+MrTheory4@users.noreply.github.com>
2023-12-19 04:56:26 +0100

    Simplificación de la clase usuarios

--- PIM PAM PUM!/usuarios.js
+++ PIM PAM PUM!/usuarios.js
@@ -6,12 +6,12 @@ var usuariosRegistrados = [];
 
 //Establecemos primero un método que nos permita crear un usuario
 //Crear un usuario
-function newUser(user) 
+function newUser(username, psw) 
 {
     $.ajax({
     method:"POST", //Se trata de una petición de tipo post, pues creamos un nuevo recurso
-    url:"http://localhost:8080/usuarios", //En el servidor definiremos el directorio /usuarios medinante @RequestMapping
-    data:JSON.stringify(user), //Pasaremos como cadena la información del user
+    url:ip.http +"/usuarios", //En el servidor definiremos el directorio /usuarios medinante @RequestMapping
+    data:JSON.stringify({nombre: username, password: psw}), //Pasaremos como cadena la información del user
     processData:false, 
     headers:{"Content-Type":"application/json"}
     //En caso de acierto, se saca por consola al usuario
@@ -22,29 +22,12 @@ function newUser(user)
         console.log("Error al crear al usuario");
     })
 }
-
 //Establecemos una función a continuación que nos permite cargar a 
-function loadUsers() {
+function logIn(username, psw){
 	$.ajax({
     method:"GET", //Se trata de una petición de tipo get, pues recuperamos recursos existentes
-    url:"http://localhost:8080/usuarios/lista", //En el servidor definiremos el directorio /usuarios medinante @RequestMapping
-    processData:false,
-    headers:{"Content-Type":"application/json"}
-    //En caso de lograr cargarse los usuarios, se sacan por consola
-    }).done(function(usuariosRegistrados) {
-		console.log("Se han cargado los usuarios");
-    //En caso de error, simplemente indicamos que ha habido un error al crear al usuario
-    }).fail(function(){
-        console.log("Error al cargar los usuarios");
-    })
-}
-
-//Establecemos una función a continuación que nos permite cargar a 
-function logIn(user) {
-	$.ajax({
-    method:"GET", //Se trata de una petición de tipo get, pues recuperamos recursos existentes
-    url:"http://localhost:8080/usuarios/lista", //En el servidor definiremos el directorio /usuarios medinante @RequestMapping
-    data:JSON.stringify(user), //Pasaremos como cadena la información del user
+    url:ip.http +"/usuarios/"+ username + "/" + psw, //En el servidor definiremos el directorio /usuarios medinante @RequestMapping
+    data:(username,psw), //Pasaremos como cadena la información del user
     processData:false,
     headers:{"Content-Type":"application/json"}
     //En caso de lograr cargarse los usuarios, se sacan por consola
@@ -62,9 +45,6 @@ $(document).ready(function()
     //Indicamos por concola que se ha cargado correctamente
     console.log('El DOM está cargado'); //Únicamente para ayudarnos
 
-    //Una vez que ya se han agregado todos los usuarios, se cargan
-	usuariosRegistrados=loadUsers();
-	
     //Una vez que se han cargado los usuarios ya existentes, se puede proceder a definir las funcionalidades para el resto de elementos
 	//Comenzaremos por el botón que nos permitirá crear un usuario CUANDO AGAMOS CLICK EN EL
 	$("#CreateUserButton").click(function () {
@@ -81,43 +61,14 @@ $(document).ready(function()
             }
             else
             {
-                //Comporbaremos ahora si el usuario ya está en uso
-                var repetido=false;//Inicializamos la correspondiente variable a false
-                for(var i=0;i<usuariosRegistrados.length;i++)
+                if(!UserName.contains(' '))
                 {
-                    //Se comprueban todos los nombres de los usuarios registrados
-                    if(UserName==usuariosRegistrados[i].nombre)
-                    {
-                        //Si coincide con alguno, se cambia el valor de la variable
-                        repetido=true;
-                    }
+                    //Usamos la función newUser para crear al usuario.
+                    newUser(UserName,UserPassword);
                 }
-                //En caso de que ese nombre ya está en uso, se pide que se introduzca otro nombre
-                if(repetido==true)
-                {
-                    console.log("Introduzca otro nombre");
-                }
-                //En caso contrario, se crea un nuevo usuario
                 else
                 {
-                    if(!UserName.contains(' '))
-                    {
-                        //Creamos un usuario en JSON
-                    var user = 
-                    {
-                        nombre: UserName,
-                        password: UserPassword,
-                        registrado: false
-                    }
-                    //Agremos el nuevo usuario al conjunto de usuarios registrados
-                    usuariosRegistrados.push(user);
-                    //Usamos la función newUser para crear al usuario.
-                    newUser(user);
-                    }
-                    else
-                    {
-                        console.log("El nombre de usuario no puede contener espacios");
-                    }
+                    console.log("El nombre de usuario no puede contener espacios");
                 }
             }
         } 
@@ -148,44 +99,18 @@ $(document).ready(function()
             }
             else
             {
-                //Ahora se comprueba toda la lista de usuarios cargados para detectar un usuario con ese nombre, que sería en el que se quiere iniciar sesión
-                for(var i=0;i<usuariosRegistrados.length;i++)
-                {
-                    //En caso de coincidir, se comprobará también si coincide la contraseña
-                    if(UserName==usuariosRegistrados[i].nombre)
-                    {
-                        //En caso de que no coincida la contraseña introducida con la real, se le indica al usuario
-                        if(UserPassword!=usuariosRegistrados[i].password)
-                        {
-                            UserPassword.log("No ha introducido la contraseña correcta");
-                        }
-                        //En caso de haber introducido la contraseña correcta
-                        else
-                        {
-                            var user = 
-                            {
-                                nombre: UserName,
-                                password: UserPassword,
-                                registrado: false
-                            }
-                            logIn(user)
-                            //Se indica al usuario que ha iniciado sesión correctamente
-                            console.log("Ha iniciado sesión con éxito");
-                            //Se desactivan las casillas de iniciar sesión y crear usuario, pues ya no serían necesarias
-                            $("#CreateUserButton").disabled=true;
-                            $("#LogInButton").disabled=true;
-                            //También se desacttivan los campos de texto
-                            $("#name").disabled=true;
-                            $("#password").disabled=true;
-                        }
-                    }
-                    //Si no hay ningún usuario con dicho nombre de usuario, se indica
-                    {
-                        console.log("Su usuario no está registrado");
-                    }
-                }
-		    }
-        }
+                    //Usamos la función newUser para crear al usuario.
+                    logIn(UserName,UserPassword);
+                    //Se indica al usuario que ha iniciado sesión correctamente
+                    console.log("Ha iniciado sesión con éxito");
+                    //Se desactivan las casillas de iniciar sesión y crear usuario, pues ya no serían necesarias
+                    $("#CreateUserButton").disabled=true;
+                    $("#LogInButton").disabled=true;
+                    //También se desacttivan los campos de texto
+                    $("#name").disabled=true;
+                    $("#password").disabled=true;
+            }
+		}
         //En caso de que no se hayan rellenado los campos, se le piden al usuario
         else
         {
@@ -196,3 +121,4 @@ $(document).ready(function()
         $("#password").val('');
     });
 })
+
