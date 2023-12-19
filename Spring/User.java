aaabeef1db9ec960150f5b0401824319c743c2fe
Andrés <116580236+MrTheory4@users.noreply.github.com>
2023-12-19 01:27:55 +0100

    Simplificada la clase User
    Se han eliminado parámetros que no van a usarse
    También se ha añadido un método para obtener toda la información del usuario

--- Spring/User.java
+++ Spring/User.java
@@ -3,9 +3,8 @@ package com.example.demo;
 public class User {
 	
 	//Definimos las variables que determinarán las carcaterísticas de un usuario
-	private String nombre; //Nombre del usuario
-	private String password; //Contraseña
-	private boolean registrado; //Booleano en que se indicará si el usuario ya forma parte del sistema
+	private String nombre; 
+	private String password; 
 	
 	//Definimos ahora el constructor con el que se definirá un usuario al registrarse
 	public User(String nombre_usuario, String password_usuario)
@@ -13,16 +12,9 @@ public class User {
 		//Asignamos los valores que se reciben al crear un usuario a las variables correspondientes
 		nombre=nombre_usuario;
 		password=password_usuario;
-		//Asignaremos un valor por defecto al parámetro registrado
-		registrado=false;
 	}
 	
-	//Se definen ahora los métodos get correspondientes para poder obtener la infromación del usuario
-	public boolean getRegistrado()
-	{
-		return this.registrado;
-	}
-	public String getUsuario()
+	public String getNombre()
 	{
 		return this.nombre;
 	}
@@ -30,36 +22,19 @@ public class User {
 	{
 		return this.password;
 	}
-	
-	//Se definen ahora lo métodos set correspondientes, con los que se modificará la información relativa al usaurio
-	//Cabe destacar que, evidentemente, no se definirá un método set para el usuario
-	//Esto se debe a que el nombre de usuairo es fijo, pudiendo cambiar solo el resto de parámetros
-	//En estos métodos se devolverá un booleano, que permita identificar al sistema si se han producido cambios o no
-	
-	public boolean setRegistrado(boolean registrado_param)
+	//Añadimos también un método get con el que se obtiene una cadena con toda la infomración
+	public String getInfo()
 	{
-		//Primero nos aseguraremos de que el usario no tenga el mismo valor para registrado que el parámetro que se está pasando, en cuyo caso no tendría sentido cambiar este parámetro
-		if(this.registrado==registrado_param)
-		{
-			return false; //No se ha modificado el valor
-		}
-		else
-		{
-			this.registrado=registrado_param;
-			return true; //En caso de que no coincidan, se modifica el valor
-		}
+		return ("Nombre de usuario: " +this.nombre+" \n Contraseña: " +this.password+ "\n");
 	}
-	public boolean setPassword(String password_param)
+	
+	//Se definen ahora los métodos set correspondientes, con los que se modificará la información relativa al usuario
+	public void setNombre(String nombre)
 	{
-		//Primero nos aseguraremos de que el usario no tenga el mismo valor para password que el parámetro que se está pasando, en cuyo caso no tendría sentido cambiar este parámetro
-		if(this.password.equals(password_param)) //Con el método equals comparamos el contenido de dos String
-		{
-			return false; //No se ha modificado el valor
-		}
-		else
-		{
-			this.password=password_param;
-			return true; //En caso de que no coincidan, se modifica el valor
-		}
+		this.password=nombre;
+	}
+	public void setPassword(String password_param)
+	{
+		this.password=password_param;
 	}
 }
