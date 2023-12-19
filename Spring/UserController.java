52e7f36d95ba6d7c87985122d65ed3f987789102
Andrés <116580236+MrTheory4@users.noreply.github.com>
2023-12-19 04:26:40 +0100

    Actualizado UserController

--- Spring/UserController.java
+++ Spring/UserController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/usuarios") //Utilizaremos la misma raíz del directorio para todos los métodos de esta clase
 public class UserController {
-
+	
 	//Crearemos un objeto de la clase UserService con la que utilizaremos sus métodos 
 	//Usaremos Autowired para que se enlace automáticamente
 	
@@ -23,11 +24,11 @@ public class UserController {
 	@PostMapping
 	public ResponseEntity<User> signUp(@RequestBody User usuario)
 	{
-		//Primero añadiremos a User_Service el usuario, en caso de que no exista uno igual
+		///Primero añadiremos a User_Service el usuario, en caso de que no exista uno igual
 		//Nos devuelve un boolean, con  el que podemos lanzar un mensaje para indicar si se ha registrado o no
-		boolean usuarioRepetido=User_Service.registrarse(usuario);
+		User usuarioRegistrado=User_Service.registrarse(usuario);
 		//En función del valor obtenido, lanzamos uno u otro mensaje
-		if(usuarioRepetido==true)
+		if(usuarioRegistrado!=null)
 		{
 			//En caso de que se halla creado, devolveremos un indicador de que se ha creado con éxito
 			// También se devuelve el correspondiente usuario con sus datos
@@ -44,18 +45,20 @@ public class UserController {
 	//Ahora determinaremos un método con el que iniciar sesión
 	//Se tratará de una operación de tipo post, pues solo requeriremos de un usuario como cuerpo de la petición para obtener el resultado requerido
 	
-	@GetMapping
-	public ResponseEntity<User> logIn(@RequestBody User usuario)
+	@GetMapping(value = "/{key}/{psw}")
+	public ResponseEntity<User> logIn(@PathVariable String key,@PathVariable String psw)
 	{
+		//Creamos un nuevo User con los valores obtenidos
+		User usuario_logIn=new User(key,psw);
 		//Se comprobará mediante el método correspondiente de User_Service que usuario y contraseña coinciden
 		//Nos devuelve un User, con  el que podemos lanzar un mensaje para indicar si se ha logeado
-		User usuarioObtenido=User_Service.inicio_sesion(usuario);
+		User usuarioObtenido=User_Service.inicio_sesion(usuario_logIn);
 		//En función del valor obtenido, lanzamos uno u otro pensaje
 		if(usuarioObtenido!=null)
 		{
 			//En caso de que se halla accedido correctamente, devolveremos un indicador de que se ha creado con éxito
 			// También se devuelve el correspondiente usuario con sus datos
-			return new ResponseEntity<>(usuario, HttpStatus.OK);
+			return new ResponseEntity<>(usuario_logIn, HttpStatus.OK);
 		}
 		else
 		{
@@ -65,51 +68,74 @@ public class UserController {
 		}
 	}
 	
-	@GetMapping("/lista")
-	public List<User> loadUsers()
+	@DeleteMapping(value = "/{key}/{psw}")
+	public ResponseEntity<User> deleteUser(@PathVariable String key,@PathVariable String psw)
 	{
-		List<User> userList= User_Service.getUsuarios();
-		return userList;
+		//Creamos un nuevo User con los valores obtenidos
+		User usuario_delete=new User(key,psw);
+		//Se comprobará mediante el método correspondiente de User_Service que usuario y contraseña coinciden
+		//Nos devuelve un User, con  el que podemos lanzar un mensaje para indicar si se ha logeado
+		User usuarioBorrado=User_Service.borrarUsuario(usuario_delete);
+		//En función del valor obtenido, lanzamos uno u otro pensaje
+		if(usuarioBorrado!=null)
+		{
+			//En caso de que se halla accedido correctamente, devolveremos un indicador de que se ha creado con éxito
+			// También se devuelve el correspondiente usuario con sus datos
+			return new ResponseEntity<>(usuarioBorrado, HttpStatus.OK);
+		}
+		else
+		{
+			//En caso de que no haya podido obtenerse se devuelve un mensaje de error
+			//Este indicará al usuario que se ha llevado a cabo de forma incorrecta la petición
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
 	}
-
 	
 	//A continuación, se definirá un método que permititrá cambiar la contraseña de un jugador
 	@PutMapping
 	public ResponseEntity<User> changePassword(@PathVariable String psw, @RequestBody User usuario)
 	{
-		User usuarioObtenido= User_Service.getUser(usuario.getUsuario());
-		//Ahora devolveremos una u otra ifnormación en caso de que el usuario exista o no
-		if(usuarioObtenido!=null)
+		//Modificiamos el usuario recibido con la contraseña nueva
+		usuario.setPassword(psw);
+		//Se comprobará mediante el método correspondiente de User_Service que usuario y contraseña coinciden
+		//Nos devuelve un User, con  el que podemos lanzar un mensaje para indicar si se ha logeado
+		User usuarioModificado=User_Service.cambiarPsw(usuario);
+		//En función del valor obtenido, lanzamos uno u otro pensaje
+		if(usuarioModificado!=null)
 		{
-			//Ahora comprobaremos si coinciden las contraseñas o no
-			if(usuario.getPassword().equals(psw))
-			{
-				usuarioObtenido.setPassword(usuario.getPassword());
-				return new ResponseEntity<>(usuarioObtenido,HttpStatus.OK);
-			}
-			else
-			{
-				return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-			}
+			//En caso de que se halla accedido correctamente, devolveremos un indicador de que se ha creado con éxito
+			// También se devuelve el correspondiente usuario con sus datos
+			return new ResponseEntity<>(usuarioModificado, HttpStatus.OK);
 		}
 		else
 		{
+			//En caso de que no haya podido obtenerse se devuelve un mensaje de error
+			//Este indicará al usuario que se ha llevado a cabo de forma incorrecta la petición
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		}
-		
+		}		
 	}
-
-	@DeleteMapping
-	public ResponseEntity<User> deleteUser(@PathVariable String psw,@RequestBody User usuario)
+	
+	//Ahora determinaremos un método con el que cerrar sesión
+	//Se tratará de una operación de tipo post, pues solo requeriremos de un usuario como cuerpo de la petición
+	@PostMapping(value="/logOut")
+	public ResponseEntity<User> logOut(@RequestBody User usuario)
 	{
-		boolean borrado=User_Service.deleteUser(usuario.getUsuario(), psw);
-		if(borrado)
+		//Se comprobará mediante el método correspondiente de User_Service que usuario y contraseña coinciden
+		//Nos devuelve un User, con  el que podemos lanzar un mensaje para indicar si se ha logeado
+		User usuarioDeslogeado=User_Service.cierreSesion(usuario);
+		//En función del valor obtenido, lanzamos uno u otro pensaje
+		if(usuarioDeslogeado!=null)
 		{
-			return new ResponseEntity<>(usuario,HttpStatus.OK);
+			//En caso de que se halla accedido correctamente, devolveremos un indicador de que se ha creado con éxito
+			// También se devuelve el correspondiente usuario con sus datos
+			return new ResponseEntity<>(usuarioDeslogeado, HttpStatus.OK);
 		}
 		else
 		{
+			//En caso de que no haya podido obtenerse se devuelve un mensaje de error
+			//Este indicará al usuario que se ha llevado a cabo de forma incorrecta la petición
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		}
+		}		
 	}
+	
 }
