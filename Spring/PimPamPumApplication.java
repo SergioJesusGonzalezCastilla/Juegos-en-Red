21d6235530995722c1e00b72844d666a8e45075b
Andrés <116580236+MrTheory4@users.noreply.github.com>
2023-12-29 01:44:32 +0100

    Cambios en la aplicación
    Soporte con websockets

--- Spring/PimPamPumApplication.java
+++ Spring/PimPamPumApplication.java
@@ -2,10 +2,25 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @SpringBootApplication
+@EnableWebSocket
 public class PimPamPumApplication {
 
+	public void registerWebSocketHandlers(
+			WebSocketHandlerRegistry registry) {
+		registry.addHandler(messageHandler(), "/echo").setAllowedOrigins("*");		
+	}
+	@Bean
+	public WebSocketHandler messageHandler() {
+		return new WebSocketHandlerPimPamPum();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PimPamPumApplication.class, args);
 	}
