58c439907940b15ad9235e1d3e819fddae65b69e
raulsg0108 <146940314+raulsg0108@users.noreply.github.com>
2023-12-31 13:24:06 +0100

    Actualizar jugador

--- Spring/Player.java
+++ Spring/Player.java
@@ -1,15 +1,31 @@
 package com.example.demo;
 
+import org.springframework.web.socket.WebSocketSession;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Player {
-    private String userId;
+	
+	private String userId;
+    private WebSocketSession session;
 
-    public Player(String userId) {
+    public Player(String userId, WebSocketSession session) {
         this.userId = userId;
+        this.session = session;
     }
 
     public String getUserId() {
         return userId;
     }
+    
+    public WebSocketSession getSession() {
+        return session;
+    }
 }
+
+Como podría implementar las funcionalidades necesarias en el siguiente método del servidor?:
+@Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        //Se manejan los mensajes recibidos desde el cliente
+        String payload = message.getPayload();       
+    }
