be863a33d13c17597bfd8b637945f1172e987983
Andrés <116580236+MrTheory4@users.noreply.github.com>
2023-12-31 13:24:19 +0100

    Actualizada la lógica websocket del servidor

--- Spring/WebSocketHandlerPimPamPum.java
+++ Spring/WebSocketHandlerPimPamPum.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.CloseStatus;
@@ -9,11 +11,14 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Controller
 public class WebSocketHandlerPimPamPum extends TextWebSocketHandler {
 
 	// Mapa con el que realizaremos un seguimiento de los ususarios conectados
-    private HashMap<String, Player> usuariosConectados = new HashMap<>();
+    private ConcurrentHashMap<String, Player> usuariosConectados = new ConcurrentHashMap<>();
     
 	//Método que obtiene el ID de la sesión
 	private String obtenerId(WebSocketSession session) {
@@ -42,7 +47,7 @@ public class WebSocketHandlerPimPamPum extends TextWebSocketHandler {
         // En caso de haber obtenido un ID
         if (userId != null) {
             // Creamos un objeto de la clase player con el nuevo ID
-            Player player = new Player(userId);
+            Player player = new Player(userId, session);
 
             // Almacenar el nuevo jugador en el mapa
             usuariosConectados.put(userId, player);
@@ -58,8 +63,23 @@ public class WebSocketHandlerPimPamPum extends TextWebSocketHandler {
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        //Se manejan los mensajes recibidos desde el cliente
-        String payload = message.getPayload();       
+        //Recibe el mensaje desde el cliente:   	
+        String payload = message.getPayload(); 
+        try {
+            //Se lee el mensaje recibido en JSON
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(payload);
+
+            //Envíamos el mensaje a los usuarios
+            notificarActualizacion(session, jsonNode);
+            
+            //En caso de haber algún problema durante la lectura
+        } catch (IOException e) {
+        	//Indicamos lo correspondiente
+        	System.out.println("Se ha producido un error");
+        	//Imprimimos el error
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -76,5 +96,31 @@ public class WebSocketHandlerPimPamPum extends TextWebSocketHandler {
             System.out.println("Conexión cerrada para un usuario no identificado");
         }
     }
+    
+ // Método que se encarga de enviar mensajes a todos los clientes con la información correspondiente
+    private void notificarActualizacion(WebSocketSession session, JsonNode jsonNode) {
+        // Enviar mensaje a todos los usuarios conectados excepto al que lo ha enviado
+    	// No sería necesario que recibiese es ainformación, ya que forma parte directamente dle juego
+    	// Y por lo tanto ya dispone de ella
+        for (Player player : usuariosConectados.values()) {
+        	//Obtenemos la sesión de cada usuario
+            WebSocketSession userSession = player.getSession();
+            // Comprobamos que los usuarios estén conectados a la hora de mandar mensajes
+            //Verificamso que no sea el que las ha enviado
+            if (userSession != null && !userSession.getId().equals(session.getId())) {
+                try {
+                	//Se les envía un mensaje a los usuarios con la información
+                    userSession.sendMessage(new TextMessage(jsonNode.toString()));
+                  //En caso de haber algún problema durante el envío de información
+                } catch (IOException e) {
+                	//Indicamos lo correspondiente
+                	System.out.println("Se ha producido un error");
+                	//Imprimimos el error
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
 }
 
