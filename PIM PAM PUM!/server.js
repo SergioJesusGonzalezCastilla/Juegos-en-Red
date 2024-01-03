d2d1303b45050933e87c527c9fa3036c370f0587
Javier <146627277+Javier169@users.noreply.github.com>
2024-01-03 13:47:06 +0100

    Agregada lógica jugadores máximos
    Agregada la lógica de conexión/desconexión a nivel de servidor para limitar el número de jugadores concurrentes a 2

--- PIM PAM PUM!/server.js
+++ PIM PAM PUM!/server.js
@@ -6,17 +6,33 @@ const servidorWebSocket = new WebSocket.Server({ server });
 
 const PORT = 8080;
 
+const MAX_PLAYERS = 2;
+let connectedPlayers = 0;
+
 servidorWebSocket.on('connection', (ws) => {
-  console.log('Nuevo cliente conectado');
-  ws.on('message', (message) => {
-    // Manejar mensajes del cliente
-    servidorWebSocket.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        // Reenviar el mensaje a todos los clientes, excepto al remitente
-        client.send(message);
-      }
+  if (connectedPlayers < MAX_PLAYERS) {
+    connectedPlayers++;
+
+    console.log(`Jugador ${connectedPlayers} conectado`);
+
+    ws.on('message', (message) => {
+      // Manejar mensajes del cliente
+      servidorWebSocket.clients.forEach((client) => {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          // Reenviar el mensaje a todos los clientes, excepto al remitente
+          client.send(message);
+        }
+      });
     });
-  });
+
+    ws.on('close', () => {
+      connectedPlayers--;
+      console.log(`Jugador desconectado, jugadores restantes: ${connectedPlayers}`);
+    });
+  } else {
+    // Si hay más de dos jugadores, cerrar la conexión inmediatamente
+    ws.close();
+  }
 });
 
 server.listen(PORT, () => {
