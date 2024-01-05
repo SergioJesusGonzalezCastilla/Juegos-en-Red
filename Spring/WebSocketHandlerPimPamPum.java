de1edf62c48cd0e7756394e14dff6dcdef370378
Andrés <116580236+MrTheory4@users.noreply.github.com>
2024-01-05 22:23:55 +0100

    Gestión de inicio de partida

--- Spring/WebSocketHandlerPimPamPum.java
+++ Spring/WebSocketHandlerPimPamPum.java
@@ -21,6 +21,7 @@ public class WebSocketHandlerPimPamPum extends TextWebSocketHandler {
 	// Mapa con el que realizaremos un seguimiento de los ususarios conectados
     private ConcurrentHashMap<String, Player> usuariosConectados = new ConcurrentHashMap<>();
     private int numUsuariosConectados=0;
+    private boolean juego_iniciado=false;
     	
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception 
@@ -35,7 +36,10 @@ public class WebSocketHandlerPimPamPum extends TextWebSocketHandler {
             // Almacenar el nuevo jugador en el mapa
             usuariosConectados.put(userId, player);
             numUsuariosConectados++;
-
+            if(numUsuariosConectados>=2)
+            {
+            	juego_iniciado=true;
+            }
             //Indicamos que hemos iniciado la conexión
             System.out.println("Usuario conectado con ID: " + userId);
         }
@@ -59,6 +63,11 @@ public class WebSocketHandlerPimPamPum extends TextWebSocketHandler {
                 // Agregamos la información de numUsuariosConectados al mensaje
                 ((ObjectNode) jsonNode).put("numUsuariosConectados", numUsuariosConectados);
             }
+          //En caso de que el tipo de mensaje sea juego_iniciado
+            if (jsonNode.has("tipo") && jsonNode.get("tipo").asText().equals("juego_iniciado")) {
+                // Agregamos la información de numUsuariosConectados al mensaje
+                ((ObjectNode) jsonNode).put("juego_iniciado", juego_iniciado);
+            }
             //Envíamos el mensaje a los usuarios
             notificarActualizacion(session, jsonNode);
             
@@ -80,6 +89,10 @@ public class WebSocketHandlerPimPamPum extends TextWebSocketHandler {
             //Indicamos que hemos cerrado la conexión
         	usuariosConectados.remove(userId);
         	numUsuariosConectados--;
+        	if(numUsuariosConectados<2)
+            {
+            	juego_iniciado=false;
+            }
             System.out.println("Conexión cerrada para el usuario con ID: " + userId);
         } else {
             // En caso de no obtener el ID
@@ -89,6 +102,7 @@ public class WebSocketHandlerPimPamPum extends TextWebSocketHandler {
     
  // Método que se encarga de enviar mensajes a todos los clientes con la información correspondiente
     private void notificarActualizacion(WebSocketSession session, JsonNode jsonNode) {
+    	// Código para enviar el JSON al cliente
         // Obtemción del tipo del mensaje
         String tipoMensaje = jsonNode.has("tipo") ? jsonNode.get("tipo").asText() : "";
 
@@ -100,6 +114,21 @@ public class WebSocketHandlerPimPamPum extends TextWebSocketHandler {
             } catch (IOException e) {
                 System.out.println("Error al enviar el mensaje al emisor");
                 e.printStackTrace();
+            }
+        }
+        else if("juego_iniciado".equals(tipoMensaje))
+        {
+        	// Si es 'juego_iniciado', envía el mensaje a todos los usuarios conectados incluso al emisor
+            for (Player player : usuariosConectados.values()) {
+                WebSocketSession userSession = player.getSession();
+                if (userSession != null) {
+                    try {
+                        userSession.sendMessage(new TextMessage(jsonNode.toString()));
+                    } catch (IOException e) {
+                        System.out.println("Se ha producido un error");
+                        e.printStackTrace();
+                    }
+                }
             }
         } else {
             // Si no es 'numero_usuarios', envía el mensaje a todos los usuarios conectados excepto al emisor
